4a8bd9685e73635928126e875e44e349bc937f90
msubirana <mb2subi@gmail.com>
2020-02-26 16:13:18 +0100

    Change of getViewpointCoordinates from Rbowtie2 based to Biostrings based
    Former-commit-id: c6cbf01671d852fd5ed385a02c9017f892d731c8 [formerly 20495eea274ac69b7558a2e0d1e1d970800472f7]
    Former-commit-id: 826175e8150947113ac3049259e44b9bfc508337
    Former-commit-id: 45942673081d9d65a61cbc42fd0e84979b4faaad
    Former-commit-id: 9d87675c6a2931932315dfa49e22c92ad76edd61
    Former-commit-id: 62c3cb9c2a3391a5987fbe03d9c8efe5eb328d99
    Former-commit-id: e81ad2555e0094f7672cdb6d58dfa69a5bb9e0b6
    Former-commit-id: f58759a2844f9f0e57fdff50db8898e6938720b8

--- R/getViewpointCoordinates.R
+++ R/getViewpointCoordinates.R
@@ -1,29 +1,27 @@
+#' Get viewpoint coordinates
+#'
+#' Finds the viewpoint coordinates for a given reference genome.
+#'
+#' @param bait_seq Character containing the bait primer sequence.
+#' @param bait_pad Character containing the pad sequence (sequence between the bait primer and the restriction enzyme sequence).
+#' @param res_enz Character containing the restriction enzyme sequence.
+#' @param ref_gen A BSgenome object of the reference genome.
+#' @examples
+#' \dontrun{
+#' getViewpointCoordinates(bait_seq="CCCAAATCGCCCAGACCAG",
+#'               bait_pad="GCGCG",
+#'               res_enz="GATC",
+#'               ref_gen=BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19)
+#' }
+#' @export
 getViewpointCoordinates <- function(bait_seq,
                                     bait_pad,
                                     res_enz,
                                     ref_gen) {
+
   viewpoint <-  paste0(bait_seq, bait_pad, res_enz)
-
-  # TODO: bowtie index autogeneration if not exist? set automatic bowtie or define path?
-  bowtie_index <- gsub('\\.fa$', '', ref_gen)
-
-  view_point_pos <- system(paste(system.file(package="Rbowtie2", "bowtie2-align-s"),
-                                 '--quiet',
-                                 '-N 0',
-                                 '-x', bowtie_index,
-                                 '-c', viewpoint),
-                           intern = T)
-
-
-  view_point_pos <- utils::tail(view_point_pos, n = 1)
-  view_point_pos <- unlist(strsplit(view_point_pos, "\t"))
-  pos_chr <- view_point_pos[3]
-  pos_start <- as.numeric(view_point_pos[4])
-  pos_end <- pos_start + nchar(viewpoint) - nchar(res_enz)
-  pos_viewpoint <- GenomicRanges::GRanges(seqnames = pos_chr,
-                                          ranges = IRanges::IRanges(start=pos_start,
-                                                                   end=pos_end)
-                                          )
+  # match viewpoint to genome
+  pos_viewpoint <- Biostrings::vmatchPattern(viewpoint, ref_gen, max.mismatch = 0)
 
   return(pos_viewpoint)
 }
