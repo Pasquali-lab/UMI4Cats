c11eb176d2dd48f6c2503f1bb23239f5aee8f101
Mireia Ramos <mireiarr9@gmail.com>
2020-03-27 17:29:56 +0100

    Fix error in plotGenes when no genes were detected or multiple ids for same tx
    Former-commit-id: ca5ab5495ce23049153dac90aa3bb9e4264388bf

--- R/createGeneAnnotation.R
+++ R/createGeneAnnotation.R
@@ -18,55 +18,62 @@ createGeneAnnotation <- function(window,
                                                   columns=c("tx_id", "tx_name",
                                                             "gene_id"))
   trans <- trans[sapply(trans$gene_id, length)>0,]
-  trans$gene_id <- unlist(trans$gene_id)
 
-  ## Select longest transcripts
-  if (longest) {
-    trans <- trans[order(trans$gene_id),]
-    trans_sp <- split(trans, trans$gene_id)
-    sel <- unlist(lapply(trans_sp, function(x) width(x) == max(width(x))))
-    trans <- trans[sel,]
-  }
+  if (length(trans) == 0) {
+    return(trans)
 
-  # If some have same width, select first example
-  reps <- table(trans$gene_id) > 1
-  reps <- names(reps)[reps]
-
-  if (length(reps) > 0) {
-    trans_uni <- c(trans[which(rowSums(sapply(reps, grepl, trans$gene_id))==0),],
-                   trans[sapply(reps, function(x) grep(x, trans$gene_id)[1])])
   } else {
-    trans_uni <- trans
+    trans$gene_id <- unlist(trans$gene_id)
+
+    ## Select longest transcripts
+    if (longest) {
+      trans <- trans[order(trans$gene_id),]
+      trans_sp <- split(trans, trans$gene_id)
+      sel <- unlist(lapply(trans_sp, function(x) width(x) == max(width(x))))
+      trans <- trans[sel,]
+    }
+
+    # If some have same width, select first example
+    reps <- table(trans$gene_id) > 1
+    reps <- names(reps)[reps]
+
+    if (length(reps) > 0) {
+      trans_uni <- c(trans[which(rowSums(sapply(reps, grepl, trans$gene_id))==0),],
+                     trans[sapply(reps, function(x) grep(x, trans$gene_id)[1])])
+    } else {
+      trans_uni <- trans
+    }
+
+    trans_uni$type <- "GENE"
+
+    ## Retrieve exons for selected transcripts
+    exons <- GenomicFeatures::exons(TxDb,
+                                    columns=c("tx_id", "tx_name", "gene_id"),
+                                    filter=list("tx_id"=trans_uni$tx_id))
+    exons$tx_id <- sapply(exons$tx_id, function(x) x[x %in% trans_uni$tx_id])
+    exons$tx_name <- sapply(exons$tx_name, function(x) x[x %in% trans_uni$tx_name])
+    exons$gene_id <- sapply(exons$gene_id, function(x) x[x %in% trans_uni$gene_id])
+
+    exons$type <- "EXON"
+
+    trans <- c(trans_uni, exons)
+    trans$gene_id <- as.character(trans$gene_id)
+    trans$tx_id <- as.character(trans$tx_id)
+    trans$tx_name <- as.character(trans$tx_name)
+
+    ## Get gene names
+    sym <- unlist(annotate::lookUp(unique(trans$gene_id),
+                                   data='org.Hs.eg',
+                                   what="SYMBOL",
+                                   load=TRUE))
+
+    df_name <- data.frame(gene_id=names(sym),
+                          gene_name=sym,
+                          stringsAsFactors = FALSE)
+
+    mcols(trans) <- dplyr::left_join(data.frame(mcols(trans)),
+                                     df_name)
+
+    return(trans)
   }
-
-  trans_uni$type <- "GENE"
-
-  ## Retrieve exons for selected transcripts
-  exons <- GenomicFeatures::exons(TxDb,
-                                  columns=c("tx_id", "tx_name", "gene_id"),
-                                  filter=list("tx_id"=trans_uni$tx_id))
-  exons$tx_id <- sapply(exons$tx_id, function(x) x[x %in% trans_uni$tx_id])
-  exons$tx_name <- sapply(exons$tx_name, function(x) x[x %in% trans_uni$tx_name])
-  exons$gene_id <- sapply(exons$gene_id, function(x) x[x %in% trans_uni$gene_id])
-
-  exons$type <- "EXON"
-
-  trans <- c(trans_uni, exons)
-
-  ## Get gene names
-  sym <- unlist(annotate::lookUp(unique(unlist(trans$gene_id)),
-                                 data='org.Hs.eg',
-                                 what="SYMBOL",
-                                 load=TRUE))
-
-  df_name <- data.frame(gene_id=names(sym),
-                        gene_name=sym,
-                        stringsAsFactors = FALSE)
-
-  mcols(trans) <- dplyr::left_join(data.frame(mcols(trans)),
-                                   df_name)
-
-  return(trans)
 }
-
-
