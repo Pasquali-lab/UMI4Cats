a4a1f1fea018ab80435ffcf249be423b75a088bb
Mireia Ramos <mireiarr9@gmail.com>
2020-04-06 11:34:40 +0200

    Fix function (classes, fdef, mtable) error

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -22,63 +22,63 @@ setGeneric("results",
                     counts=TRUE) standardGeneric("results"))
 
 
-#' @importMethodsFrom SummarizedExperiment assay
-#' @export
-setGeneric("assay")
+#' #' @importMethodsFrom SummarizedExperiment assay
+#' #' @export
+#' setGeneric("assay")
+#'
+#' #' @importMethodsFrom SummarizedExperiment assays
+#' #' @export
+#' setGeneric("assays")
 
-#' @importMethodsFrom SummarizedExperiment assays
-#' @export
-setGeneric("assays")
+#' #' @importMethodsFrom SummarizedExperiment assays<-
+#' #' @export
+#' setGeneric("assays<-")
 
-#' @importMethodsFrom SummarizedExperiment assays<-
-#' @export
-setGeneric("assays<-")
+#' #' @importMethodsFrom SummarizedExperiment colData
+#' #' @export
+#' setGeneric("colData")
+#'
+#' #' @importMethodsFrom SummarizedExperiment colData<-
+#' #' @export
+#' setGeneric("colData<-")
 
-#' @importMethodsFrom SummarizedExperiment colData
-#' @export
-setGeneric("colData")
+#' #' @importMethodsFrom S4Vectors metadata
+#' #' @export
+#' setGeneric("metadata")
+#'
+#' #' @importMethodsFrom S4Vectors metadata<-
+#' #' @export
+#' setGeneric("metadata<-")
 
-#' @importMethodsFrom SummarizedExperiment colData<-
-#' @export
-setGeneric("colData<-")
+#' #' @importMethodsFrom SummarizedExperiment rowRanges
+#' #' @export
+#' setGeneric("rowRanges")
+#'
+#' #' @importMethodsFrom SummarizedExperiment rowRanges<-
+#' #' @export
+#' setGeneric("rowRanges<-")
 
-#' @importMethodsFrom S4Vectors metadata
-#' @export
-setGeneric("metadata")
-
-#' @importMethodsFrom S4Vectors metadata<-
-#' @export
-setGeneric("metadata<-")
-
-#' @importMethodsFrom SummarizedExperiment rowRanges
-#' @export
-setGeneric("rowRanges")
-
-#' @importMethodsFrom SummarizedExperiment rowRanges<-
-#' @export
-setGeneric("rowRanges<-")
-
-#' @importFrom S4Vectors queryHits
-#' @export
-setGeneric("queryHits")
-
-#' @importFrom S4Vectors subjectHits
-#' @export
-setGeneric("subjectHits")
-
-#' @importFrom IRanges subsetByOverlaps
-#' @export
-setGeneric("subsetByOverlaps")
-
-#' @importFrom IRanges IRanges
-#' @export
-setGeneric("IRanges")
-
-#' @importFrom S4Vectors SimpleList
-#' @export
-setGeneric("SimpleList")
-
-#' @importMethodsFrom  S4Vectors elementNROWS
-#' @export
-setGeneric("elementNROWS")
-
+#' #' @importFrom S4Vectors queryHits
+#' #' @export
+#' setGeneric("queryHits")
+#'
+#' #' @importFrom S4Vectors subjectHits
+#' #' @export
+#' setGeneric("subjectHits")
+#'
+#' #' @importFrom IRanges subsetByOverlaps
+#' #' @export
+#' setGeneric("subsetByOverlaps")
+#'
+#' #' @importFrom IRanges IRanges
+#' #' @export
+#' setGeneric("IRanges")
+#'
+#' #' @importFrom S4Vectors SimpleList
+#' #' @export
+#' setGeneric("SimpleList")
+#'
+#' #' @importMethodsFrom  S4Vectors elementNROWS
+#' #' @export
+#' setGeneric("elementNROWS")
+#'
