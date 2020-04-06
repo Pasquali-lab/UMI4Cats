19c5f4dcdb482fb7d3707e61a0cb28a4dfd1717d
Mireia Ramos <mireiarr9@gmail.com>
2020-04-06 11:34:40 +0200

    Fix function (classes, fdef, mtable) error
    Former-commit-id: a4a1f1fea018ab80435ffcf249be423b75a088bb

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,6 @@
 # UMI4Cats 0.99.1
 * Fixed error in function `createGeneAnnotation` and `plotGenes` that occur when there are no genes in the region or a gene has multiple identifiers.
+* Fixed duplicated generics definition for `SummarizedExperiment` objects to avoid error when reloading the package.
 
 # UMI4Cats 0.99.0
 
--- R/globals.R
+++ R/globals.R
@@ -2,4 +2,8 @@ utils::globalVariables(c("factors", "scales", "value",
                          "hg19_gene_annoation_ensemblv75", "stepping",
                          "gene_name", "geo_coord", "grouping_var",
                          "relative_position", "sample_id", "al_mapped",
-                         "al_unmapped", "variable"))
+                         "al_unmapped", "variable",
+                         "queryHits", "subjectHits", "subsetByOverlaps",
+                         "assay", "metadata", "rowRanges", "assays", "assays<-",
+                         "assay", "colData", "IRanges", "SimpleList",
+                         "metadata<-", "rowRanges<-"))
