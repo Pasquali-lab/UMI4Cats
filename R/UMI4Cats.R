6c30b26a56a419e579bb81a04764f70116df051e
Mireia Ramos <mireiarr9@gmail.com>
2020-04-07 18:08:34 +0200

    Update docs & vignette
    Former-commit-id: 7d07e378c4b34cbd9e2b04075eb8d2afca046c26

--- R/UMI4Cats.R
+++ R/UMI4Cats.R
@@ -1,3 +1,21 @@
+#' UMI4Cats: A package for analyzing UMI-4C chromatin contact data
+#'
+#' The UMI4Cats package provides functions for the pre-processing, analysis and
+#' visualization of UMI-4C chromatin contact data.
+#'
+#' @section Pre-processing:
+#' The pre-processing functions ...
+#'
+#' @section Analysis:
+#' The analysis functions ...
+#'
+#' @section Visualization:
+#' The plotting functions ...
+#'
+#' @docType package
+#' @name UMI4Cats
+NULL
+
 utils::globalVariables(c("factors", "scales", "value",
                          "hg19_gene_annoation_ensemblv75", "stepping",
                          "gene_name", "geo_coord", "grouping_var",
