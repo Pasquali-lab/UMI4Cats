dcc8fcaae3cd46c40bbf0659fca9576fbb6966ba
Mireia Ramos <mireiarr9@gmail.com>
2020-10-19 19:47:59 +0200

    Update sample data creation to use new functions & reduce size

--- inst/scripts/CIITA_process_example.R
+++ inst/scripts/CIITA_process_example.R
@@ -22,15 +22,17 @@ hg19_dpnii <- digestGenome(
   res_enz = "GATC",
   name_RE = "DpnII",
   ref_gen = BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19,
+  sel_chr = "chr16",
   out_path = "digested_genome/"
 )
 
 ## 2) Process UMI-4C fastq files -----------------------------------------------
 raw_dir <- file.path(path, "CIITA", "fastq")
+wk_dir <- here::here("inst", "extdata", "CIITA")
 
 contactsUMI4C(
   fastq_dir = raw_dir,
-  wk_dir = "CIITA",
+  wk_dir = wk_dir,
   bait_seq = "GGACAAGCTCCCTGCAACTCA",
   bait_pad = "GGACTTGCA",
   res_enz = "GATC",
@@ -38,12 +40,13 @@ contactsUMI4C(
   digested_genome = hg19_dpnii,
   bowtie_index = file.path(path, "ref_genome", "ucsc.hg19.chr16"),
   ref_gen = BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19,
+  filter_bp = 1e6,
   threads = 10
 )
 
 ## 3) Create UMI4C object -----------------------------------------------------
 # Load sample processed file paths
-files <- list.files(file.path(path, "CIITA", "count"),
+files <- list.files(file.path(wk_dir, "count"),
                     pattern = "*_counts.tsv.gz",
                     full.names = TRUE
 )
@@ -66,5 +69,8 @@ colData <- colData %>%
 ex_ciita_umi4c <- makeUMI4C(
   colData = colData,
   viewpoint_name = "CIITA",
-  bait_expansion = 3e5
+  bait_expansion = 3e5,
+  grouping = NULL
 )
+
+usethis::use_data(ex_ciita_umi4c, overwrite=TRUE)
